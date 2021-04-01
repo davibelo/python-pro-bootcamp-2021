cece8195ffb3508261cb3af2f97b2c3f56ac7862
davibelo <davibelo@gmail.com>
2021-04-01 17:19:59 -0300

    using data from trivia DB

---
+++ 02-intermediate/01-IntroToOOP/02-Quiz/data_trivia.py
@@ -0,0 +1,36 @@
+question_trivia = [{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "FLAC stands for &quot;Free Lossless Audio Condenser&quot;&#039;",
+    "correct_answer": "False",
+    "incorrect_answers": ["True"]
+}, {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "The open source program Redis is a relational database server.",
+    "correct_answer": "False",
+    "incorrect_answers": ["True"]
+}, {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "Android versions are named in alphabetical order.",
+    "correct_answer": "True",
+    "incorrect_answers": ["False"]
+}, {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "difficulty": "medium",
+    "question": "Linus Sebastian is the creator of the Linux kernel, which went on to be used in Linux, Android, and Chrome OS.",
+    "correct_answer": "False",
+    "incorrect_answers": ["True"]
+}]
--- 02-intermediate/01-IntroToOOP/02-Quiz/main.py
+++ 02-intermediate/01-IntroToOOP/02-Quiz/main.py
@@ -1,13 +1,25 @@
 from question_model import Question
 from data import question_data
+from data_trivia import question_trivia
 from quiz_brain import QuizBrain
 
 question_bank = []
-for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
+
+# using data 
+# for question in question_data:
+#     question_text = question["text"]
+#     question_answer = question["answer"]
+#     new_question = Question(question_text, question_answer)
+#     question_bank.append(new_question)
+
+# using data_trivia
+for question in question_trivia:
+      question_text = question["question"]
+      question_answer = question["correct_answer"]
+      new_question = Question(question_text, question_answer)
+      question_bank.append(new_question)
+
+
 
 quiz = QuizBrain(question_bank)
 # print(help(QuizBrain))
