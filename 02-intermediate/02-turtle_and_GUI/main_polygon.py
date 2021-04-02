c9fdb17ee0312dee03c3fb1fc8cc0dbdd2e59af9
davibelo <davibelo@gmail.com>
2021-04-02 13:32:25 -0300

    organizing

--- 02-intermediate/02-turtle_and_GUI/main_polygon.py
+++ 02-intermediate/02-turtle_and_GUI/main_polygon.py
@@ -4,21 +4,6 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 
-tim.shape("turtle")
-tim.color("#457b9d")
-
-# drawing a square
-# for _ in range(4):
-#     tim.forward(100)
-#     tim.right(90)
-
-# drawing a dash line
-# for _ in range(15):
-#     tim.forward(10)
-#     tim.penup()
-#     tim.forward(10)
-#     tim.pendown()
-
 
 def get_N_HexCol(N=5):
     """generate a list of colors"""
