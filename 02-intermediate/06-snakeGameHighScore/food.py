09712fb3bf6fed6662a9bca3947c4024bd8b15a5
davibelo <davibelo@gmail.com>
2021-04-06 22:06:16 -0300

    high score feature

---
+++ 02-intermediate/06-snakeGameHighScore/food.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("turtle")
+        self.color("blue")
+        self.shapesize(stretch_wid=1, stretch_len=1)
+        self.speed("fastest")
+        self.refresh()        
+
+    def refresh(self):
+        random_x = random.choice(range(-200, 200, 20))
+        random_y = random.choice(range(-200, 200, 20))
+        self.goto(random_x, random_y)
---
+++ 02-intermediate/06-snakeGameHighScore/main.py
@@ -0,0 +1,47 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+# event listeners to detect key strokes
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    snake.move()
+
+    # detecting collision with food
+    if snake.head.distance(food) < 20:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # detect collision with a wall
+    if abs(snake.head.xcor()) > 280 or abs(snake.head.ycor()) > 280:        
+        scoreboard.reset()
+        snake.reset()
+
+    # detect collision with itself
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:            
+            scoreboard.reset()
+            snake.reset()
+
+screen.exitonclick()
