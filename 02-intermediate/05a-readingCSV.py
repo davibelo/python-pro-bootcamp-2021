88b6a919b276361199e0fc93ce7dbcb4a6d73adf
davibelo <davibelo@gmail.com>
2021-04-08 09:25:19 -0300

    csv example

--- 02-intermediate/05a-readingCSV.py
+++ 02-intermediate/05a-readingCSV.py
@@ -3,17 +3,32 @@ import pandas as pd
 
 import csv
 
+# with csv
 with open("02-intermediate/05b-weather_data.csv") as data_file:
-    data = csv.reader(data_file)
+    data0 = csv.reader(data_file)
     temperatures = []
-    for row in data:
+    for row in data0:
         if row[1] != "temp":
             temperatures.append(int(row[1]))
 
-print(temperatures)
+print("a)", temperatures)
 
+# with pandas
+data = pd.read_csv("02-intermediate/05b-weather_data.csv")
+print("b)", data)
+print("c)", type(data))
+print("d)", data["temp"])
 
-data2 = pd.read_csv("02-intermediate/05b-weather_data.csv")
-print(data2)
-print(type(data2))
-print(data2["temp"])
+data_dict = data["temp"].to_dict()
+print("e)", data_dict)
+
+temp_list = data["temp"].to_list()
+print("f)", temp_list)
+
+average = sum(temp_list) / len(temp_list)
+print("g)", average)
+
+# with panda mean method
+print("h)", type(data["temp"]))
+avg = data["temp"].mean()
+print("i)", avg)
