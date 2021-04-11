56c5a5d0258a714a68e5a8026ed78116d1fff835
davibelo <davibelo@gmail.com>
2021-04-11 19:22:06 -0300

    exercise 2

--- 02-intermediate/07-dictionaryComprehension.py
+++ 02-intermediate/07-dictionaryComprehension.py
@@ -20,9 +20,24 @@ passed_students = {
 }
 print(f"passed students: {passed_students}")
 
-# exercise:
+# exercise 1:
 # create a dict with each word and its number of letters for the sentence
 sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
-words = sentence.split()
-new_dict = {word: len(word) for word in words}
+new_dict = {word: len(word) for word in sentence.split()}
 print(new_dict)
+
+# exercise 2:
+# convert a dict of temp in Celsius to a dict with temp in fahrenheit
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+weather_f = {day: (temp_c * 9/5) + 32 for (day, temp_c) in weather_c.items()}
+print(weather_f)
