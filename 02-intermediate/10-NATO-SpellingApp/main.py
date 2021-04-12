16f40e49300f2e4e30cdc8df2d3ebed4795544a5
davibelo <davibelo@gmail.com>
2021-04-11 21:01:27 -0300

    nato spelling app

---
+++ 02-intermediate/10-NATO-SpellingApp/main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+# 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"...}
+
+# reading csv file
+alpha_file = "02-intermediate/10-NATO-SpellingApp/nato_phonetic_alphabet.csv"
+df_alpha = pd.read_csv(alpha_file)
+
+# creating nato alphabet dict
+alpha_dict = {row.letter: row.code for (
+    index, row) in df_alpha.iterrows()}
+
+# 2. Create a list of the phonetic code words from a word that the user inputs.
+
+# getting a word from user
+word = input("Type a word: ").upper()
+
+# creating and printing the list
+spelling_list = [alpha_dict[letter] for letter in word]
+print(spelling_list)
