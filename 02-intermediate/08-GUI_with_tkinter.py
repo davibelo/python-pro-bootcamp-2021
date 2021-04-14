a476433a94284d6e518f1021ea52583aab6d19e0
davibelo <davibelo@gmail.com>
2021-04-13 21:43:46 -0300

    tkinter start

--- 02-intermediate/08-GUI_with_tkinter.py
+++ 02-intermediate/08-GUI_with_tkinter.py
@@ -7,7 +7,8 @@ window.title("My tkinter app")
 window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label.pack(side="left")
 
 # calling the main loop that listener for user interaction
 window.mainloop()
+ 
