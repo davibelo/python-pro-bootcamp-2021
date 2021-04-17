7ca85abe20061f7bf103b02334510d3a55c03254
davibelo <davibelo@gmail.com>
2021-04-17 20:13:50 -0300

    tkinter example improved

--- 02-intermediate/08-GUI_with_tkinter.py
+++ 02-intermediate/08-GUI_with_tkinter.py
@@ -1,14 +1,121 @@
-import tkinter
+from tkinter import *
+# https://docs.python.org/3/library/tkinter.html
+# https://en.wikipedia.org/wiki/Tk_(software)
+# https://www.tcl.tk/man/tcl8.7/contents.htm
 
-# creating a window object
-window = tkinter.Tk()
+# a tkinter tutorial
+# https://tkdocs.com/tutorial/firstexample.html
 
+# Window object
+window = Tk()
 window.title("My tkinter app")
-window.minsize(width=500, height=300)
+window.minsize(width=500, height=500)
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+# Label
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
 
+# changing the object argument after creation:
+my_label["text"] = "This is a label"
+my_label.config(text="This is a label")
+
+# Entry
+# https://www.tcl.tk/man/tcl8.7/TkCmd/entry.htm
+entry = Entry(width=30)
+entry.insert(END, string="Some text to begin with.")
+# gets text in entry
+print(entry.get())
+entry.pack()
+
+# Button
+
+
+def button_clicked():
+    """function to be called when button is clicked"""
+    new_text = entry.get()
+    my_label.config(text=new_text)
+    print("I got clicked")
+
+
+button = Button(text="click me", command=button_clicked)
+button.pack()
+
+# Text
+text = Text(height=5, width=30)
+# puts cursor in textbox.
+text.focus()
+# adds some text to begin with.
+text.insert(END, "Example of multi-line text entry.")
+# get current value in textbox at line 1, character 0
+print(text.get("1.0", END))
+text.pack()
+
+
+# Spinbox
+
+
+def spinbox_used():
+    # gets the current value in spinbox.
+    print(spinbox.get())
+
+
+spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+spinbox.pack()
+
+# Scale
+# Called with current scale value.
+
+
+def scale_used(value):
+    print(value)
+
+
+scale = Scale(from_=0, to=100, command=scale_used)
+scale.pack()
+
+# Checkbutton
+
+
+def checkbutton_used():
+    # Prints 1 if On button checked, otherwise 0.
+    print(checked_state.get())
+
+
+# variable to hold on to checked state, 0 is off, 1 is on.
+checked_state = IntVar()
+checkbutton = Checkbutton(
+    text="Is On?", variable=checked_state, command=checkbutton_used)
+checked_state.get()
+checkbutton.pack()
+
+# Radiobutton
+
+
+def radio_used():
+    print(radio_state.get())
+
+
+# Variable to hold on to which radio button value is checked.
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Option1", value=1,
+                           variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Option2", value=2,
+                           variable=radio_state, command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
+
+# Listbox
+def listbox_used(event):
+    # Gets current selection from listbox
+    print(listbox.get(listbox.curselection()))
+
+
+listbox = Listbox(height=4)
+fruits = ["Apple", "Pear", "Orange", "Banana"]
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+listbox.bind("<<ListboxSelect>>", listbox_used)
+listbox.pack()
 # calling the main loop that listener for user interaction
 window.mainloop()
- 
