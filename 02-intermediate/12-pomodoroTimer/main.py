9a4bdfba35d2273f2a03ec300e743c15123e0373
davibelo <davibelo@gmail.com>
2021-04-18 11:45:45 -0300

    UI done

--- 02-intermediate/12-pomodoroTimer/main.py
+++ 02-intermediate/12-pomodoroTimer/main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # -------------------------- CONSTANTS ---------------------------- #
 PINK = "#e2979c"
@@ -9,10 +10,37 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ------------------------- TIMER RESET --------------------------- # 
+# ------------------------- TIMER RESET --------------------------- #
 
-# ------------------------- TIMER MECHANISM ----------------------- # 
+# ------------------------- TIMER MECHANISM ----------------------- #
 
-# ------------------------- COUNTDOWN MECHANISM ------------------- # 
+# ------------------------- COUNTDOWN MECHANISM ------------------- #
 
 # ------------------------- UI SETUP ------------------------------ #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+title_label = Label(text="Timer", bg=YELLOW, fg=GREEN,
+                    font=(FONT_NAME, 32, "bold"))
+title_label.grid(row=0, column=1)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="02-intermediate/12-pomodoroTimer/tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(101, 130, text="00:00", fill="white",
+                   font=(FONT_NAME, 24, "bold"))
+canvas.grid(row=1, column=1)
+
+start_button = Button(text="Start")
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset")
+reset_button.grid(row=2, column=2)
+
+check_label = Label(text="✔", bg=YELLOW, fg=GREEN,
+                    font=(FONT_NAME, 20, "bold"))
+check_label.grid(row=3, column=1)
+
+
+window.mainloop()
