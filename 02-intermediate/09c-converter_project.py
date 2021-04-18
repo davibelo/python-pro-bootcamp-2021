93a08f506c4333a1191a024bbb0c32b290cb7620
davibelo <davibelo@gmail.com>
2021-04-17 23:47:56 -0300

    convert project complete

--- 02-intermediate/09c-converter_project.py
+++ 02-intermediate/09c-converter_project.py
@@ -1,27 +1,31 @@
 from tkinter import *
 
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = miles * 1.609
+    result_label.config(text=str(km))
+
 window = Tk()
 window.title("Miles to km converter")
 window.minsize(width=100, height=100)
-window.config(padx=10, pady=10)
+window.config(padx=20, pady=20)
 
-entry = Entry()
-entry.grid(row=0, column=1)
+miles_input = Entry(width=7)
+miles_input.grid(row=0, column=1)
 
-miles_label = Label()
-miles_label.config(text="Miles")
+miles_label = Label(text="Miles")
 miles_label.grid(row=0, column=2)
 
-equal_label = Label()
-equal_label.config(text="is qual to:")
+equal_label = Label(text="is qual to:")
 equal_label.grid(row=1, column=0)
 
-result_label = Label()
-result_label.config(text="Km")
-result_label.grid(row=1, column=2)
+result_label = Label(text=0)
+result_label.grid(row=1, column=1)
 
-calculate_button = Button()
-calculate_button.config(text="Calculate")
+km_label = Label(text="Km")
+km_label.grid(row=1, column=2)
+
+calculate_button = Button(text="Calculate", command=miles_to_km)
 calculate_button.grid(row=2, column=1)
 
 window.mainloop()
