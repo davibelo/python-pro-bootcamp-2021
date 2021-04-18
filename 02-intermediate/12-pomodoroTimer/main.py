1f757ed4c8753e1d7cf386f91882ac95d332656c
davibelo <davibelo@gmail.com>
2021-04-18 12:41:07 -0300

    count function

--- 02-intermediate/12-pomodoroTimer/main.py
+++ 02-intermediate/12-pomodoroTimer/main.py
@@ -16,6 +16,13 @@ LONG_BREAK_MIN = 20
 
 # ------------------------- COUNTDOWN MECHANISM ------------------- #
 
+
+def count_down(count):    
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
+
 # ------------------------- UI SETUP ------------------------------ #
 window = Tk()
 window.title("Pomodoro")
@@ -28,9 +35,11 @@ title_label.grid(row=0, column=1)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="02-intermediate/12-pomodoroTimer/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(101, 130, text="00:00", fill="white",
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white",
                    font=(FONT_NAME, 24, "bold"))
 canvas.grid(row=1, column=1)
+
+count_down(5)
 
 start_button = Button(text="Start")
 start_button.grid(row=2, column=0)
@@ -38,9 +47,9 @@ start_button.grid(row=2, column=0)
 reset_button = Button(text="Reset")
 reset_button.grid(row=2, column=2)
 
-check_label = Label(text="✔", bg=YELLOW, fg=GREEN,
+check_marks = Label(text="✔✔", bg=YELLOW, fg=GREEN,
                     font=(FONT_NAME, 20, "bold"))
-check_label.grid(row=3, column=1)
+check_marks.grid(row=3, column=1)
 
 
 window.mainloop()
