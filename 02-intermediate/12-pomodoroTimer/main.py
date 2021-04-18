61a89a9fff7f19e4c029f30603d57b45307d7ffc
davibelo <davibelo@gmail.com>
2021-04-18 16:45:25 -0300

    pomodoro timer done

--- 02-intermediate/12-pomodoroTimer/main.py
+++ 02-intermediate/12-pomodoroTimer/main.py
@@ -10,14 +10,45 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 # ------------------------- TIMER RESET --------------------------- #
+
+
+def reset_timer():
+    # stop the timer
+    window.after_cancel(timer)
+    title_label.config(text="Timer")
+    # timer_text was created with canvas, so config is different
+    canvas.itemconfig(timer_text, text="00:00")
+    check_marks.config(text="")
+    global reps
+    reps = 0
+
 
 # ------------------------- TIMER MECHANISM ----------------------- #
 
 
 def start_timer():
-    count_down(5*60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    # 8th repetition
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="BREAK...", fg=RED)
+    # 2, 4, 6th repetitions
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="BREAK", fg=PINK)
+    # 1, 3, 5, 7th repetitions
+    else:
+        count_down(work_sec)
+        title_label.config(text="WORK!", fg=GREEN)
+
 
 # ------------------------- COUNTDOWN MECHANISM ------------------- #
 
@@ -25,11 +56,21 @@ def start_timer():
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
-    if count_sec == 0:
-        count_sec = "00"
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        # window.after method works as a timer
+        global timer
+        timer = window.after(1000, count_down, count - 240)
+    else:
+        # count = 0
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
 
 
 # ------------------------- UI SETUP ------------------------------ #
@@ -52,10 +93,10 @@ canvas.grid(row=1, column=1)
 start_button = Button(text="Start", command=start_timer)
 start_button.grid(row=2, column=0)
 
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", command=reset_timer)
 reset_button.grid(row=2, column=2)
 
-check_marks = Label(text="✔✔", bg=YELLOW, fg=GREEN,
+check_marks = Label(text="", bg=YELLOW, fg=GREEN,
                     font=(FONT_NAME, 20, "bold"))
 check_marks.grid(row=3, column=1)
 
