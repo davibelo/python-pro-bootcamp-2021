65538e8822c5cd25ec9da09caae8202bb3aad9b4
davibelo <davibelo@gmail.com>
2021-04-19 21:01:19 -0300

    save function

--- 02-intermediate/13-passwordManager/main.py
+++ 02-intermediate/13-passwordManager/main.py
@@ -4,11 +4,18 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD --------------------------- #
 
+def save():
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()    
+    with open("02-intermediate/13-passwordManager/data.txt", mode="a") as file:
+        file.write(f"\n{website} | {username} | {password}")
+
 # ---------------------------- UI SETUP -------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=30, pady=30)
 
 canvas = Canvas(width=200, height=200)
 locker_img = PhotoImage(file="02-intermediate/13-passwordManager/logo.png")
@@ -26,17 +33,20 @@ password_label.grid(row=3, column=0)
 # entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 username_entry = Entry(width=35)
 username_entry.grid(row=2, column=1, columnspan=2)
+username_entry.insert(0, "davibelo@gmail.com")
 password_entry = Entry(width=18)
 password_entry.grid(row=3, column=1)
 
 # buttons
 generate_password_button = Button(
-    text="Generate Password", width=17, font=("Arial", 8, "normal"))
+    text="Generate Password", width=14, font=("TkDefaultFont", 8, "normal"))
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=43, font=("Arial", 8, "normal"))
+add_button = Button(text="Add", width=38, font=("TkDefaultFont", 8, "normal"), command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+
 
 # mainloop
 window.mainloop()
