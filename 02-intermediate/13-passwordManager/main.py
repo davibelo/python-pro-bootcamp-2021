5b45df34b307f3e32d0105e6d14399a472e150b8
davibelo <davibelo@gmail.com>
2021-04-19 21:57:20 -0300

    password manager complete

--- 02-intermediate/13-passwordManager/main.py
+++ 02-intermediate/13-passwordManager/main.py
@@ -1,10 +1,33 @@
 from tkinter import *
 # messagebox isn't a class but another module
 from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip 
 
 # ---------------------------- PASSWORD GENERATOR ---------------------- #
 
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+    
+
 # ---------------------------- SAVE PASSWORD --------------------------- #
+
 
 def save():
     website = website_entry.get()
@@ -26,6 +49,7 @@ def save():
             password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP -------------------------------- #
+
 
 window = Tk()
 window.title("Password Manager")
@@ -56,10 +80,18 @@ password_entry.grid(row=3, column=1)
 
 # buttons
 generate_password_button = Button(
-    text="Generate Password", width=14, font=("TkDefaultFont", 8, "normal"))
+    text="Generate Password",
+    width=14,
+    font=("TkDefaultFont", 8, "normal"),
+    command=generate_password
+)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=38, font=(
-    "TkDefaultFont", 8, "normal"), command=save)
+add_button = Button(
+    text="Add",
+    width=38,
+    font=("TkDefaultFont", 8, "normal"),
+    command=save
+)
 add_button.grid(row=4, column=1, columnspan=2)
 
 # mainloop
