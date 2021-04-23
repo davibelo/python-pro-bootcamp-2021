81f1c88ff9b2bcc409dbf1833430f755a595adf0
davibelo <davibelo@gmail.com>
2021-04-23 08:42:14 -0300

    comments

--- 02-intermediate/16-happyBirthdayBot/main.py
+++ 02-intermediate/16-happyBirthdayBot/main.py
@@ -5,18 +5,21 @@ import datetime as dt
 import random
 import smtplib
 
-load_dotenv(dotenv_path="02-intermediate/16-happyBirthdayBot/.env")
-PLACEHOLDER = "[NAME]"
-MY_EMAIL = "davibelo.bot@gmail.com"
-EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
-print(EMAIL_PASSWORD)
-
 # Exercise
 # 1. Update the birthdays.csv
 # 2. Check if today matches a birthday in the birthdays.csv
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 # 4. Send the letter generated in step 3 to that person's email address.
 
+# constants
+PLACEHOLDER = "[NAME]"
+MY_EMAIL = "davibelo.bot@gmail.com"
+
+# loading email password from .env file
+load_dotenv(dotenv_path="02-intermediate/16-happyBirthdayBot/.env")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+# reading birthdays from csv
 df = pd.read_csv("02-intermediate/16-happyBirthdayBot/birthdays.csv")
 
 # getting today information
@@ -24,7 +27,8 @@ now = dt.datetime.now()
 month = now.month
 day = now.day
 
-for (index, row) in df.iterrows():
+# looping through each person on data frame
+for row in df.iterrows():
     if row.month == month and row.day == day:
         
         # getting birthday person data
@@ -42,6 +46,7 @@ for (index, row) in df.iterrows():
             letter_final = letter_contents.replace(PLACEHOLDER, name)
         
         # send email with final letter
+        print("sending email...")
         with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
             connection.starttls()
             connection.login(user=MY_EMAIL, password=EMAIL_PASSWORD)
@@ -50,4 +55,5 @@ for (index, row) in df.iterrows():
                 to_addrs=email,
                 msg=f"Subject:Happy Birthday!\n\n{letter_final}"
             )
+        print("email sent!")
 
