d63a60c907e4fda87657017fabef30655fbd74bf
davibelo <davibelo@gmail.com>
2021-04-23 14:57:24 -0300

    motivation bot changes

--- 02-intermediate/15-motivationBot/main.py
+++ 02-intermediate/15-motivationBot/main.py
@@ -5,23 +5,36 @@ import datetime as dt
 import random
 
 
+# loading constants
+RELATIVE_PATH = "02-intermediate/15-motivationBot"
+MY_EMAIL = "davibelo.bot@gmail.com"
+load_dotenv(dotenv_path=f"{RELATIVE_PATH}/.env")
+MY_EMAIL_PASSWORD = os.getenv("MY_EMAIL_PASSWORD")
+print(MY_EMAIL_PASSWORD)
+
+# getting today data
 now = dt.datetime.now()
 weekday = now.weekday()
 
-if weekday == 1:
-    with open("02-intermediate/15-mailBot/quotes.txt") as quotes_file:
+# checking day of the week
+if weekday == 4:
+    # choosing a random motivation quote
+    with open(f"{RELATIVE_PATH}/quotes.txt") as quotes_file:
         quotes_list = quotes_file.readlines()
     quote = random.choice(quotes_list)
 
-load_dotenv(dotenv_path="02-intermediate/15-mailBot/.env")
-MY_EMAIL = "davibelo.bot@gmail.com"
-EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+    # getting destination information
+    name = "Davi Belo"
+    email = "davibelo@gmail.com"
 
-with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
-    connection.starttls()
-    connection.login(user=MY_EMAIL, password=EMAIL_PASSWORD)
-    connection.sendmail(
-        from_addr=MY_EMAIL,
-        to_addrs="davi.belo@outlook.com",
-        msg=f"Subject:Monday motivation!\n\n{quote}"
-    )
+    # sending email with motivation
+    print("sending email...")
+    with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_EMAIL_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=email,
+            msg=f"Subject:Monday motivation!\n\nDear {name},\n\n{quote}\n\nHave a great week!\n\nDaviBot"
+        )
+    print("email sent!")
