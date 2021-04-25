2146f342b7d79a2327df219bc4e22acd2db67641
davibelo <davibelo@gmail.com>
2021-04-25 16:23:18 -0300

    UI

--- 02-intermediate/19-TriviaQuizApp/ui.py
+++ 02-intermediate/19-TriviaQuizApp/ui.py
@@ -4,7 +4,6 @@ import os
 import pandas
 
 THEME_COLOR = "#375362"
-
 REL_PATH = "02-intermediate/19-TriviaQuizApp/"
 
 
@@ -12,26 +11,32 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
-        self.window.config(bg=THEME_COLOR)
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
         self.score_label = Label(text=" Score:0",
                                  fg="white",
                                  bg=THEME_COLOR)
-        self.score_label.grid(row=0, column=1, padx=20, pady=20)
+        self.score_label.grid(row=0, column=1)
+
         self.canvas = Canvas(width=300, height=250,
                              bg="white", highlightthickness=0)
         self.question_text = self.canvas.create_text(
             150, 125,
             text="test",
-            font=("Ariel", 20, "italic"))
-        self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
+            font=("Ariel", 20, "italic"),
+            fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
         true_img = PhotoImage(
             file=f"{REL_PATH}images/true.png")
         self.true_button = Button(
             image=true_img, highlightthickness=0)
-        self.true_button.grid(row=2, column=0, padx=20, pady=20)
+        self.true_button.grid(row=2, column=0)
+
         false_img = PhotoImage(
             file=f"{REL_PATH}images/false.png")
         self.false_button = Button(
             image=false_img, highlightthickness=0)
-        self.false_button.grid(row=2, column=1, padx=20, pady=20)
+        self.false_button.grid(row=2, column=1)
+
         self.window.mainloop()
