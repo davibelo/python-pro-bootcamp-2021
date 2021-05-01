40ff9fc9d9bfea6b3496084d0c6b27498a2bc165
davibelo <davibelo@gmail.com>
2021-05-01 20:12:43 -0300

    get missing iata codes

--- 02-intermediate/24-flight-deals-start/data_manager.py
+++ 02-intermediate/24-flight-deals-start/data_manager.py
@@ -10,20 +10,22 @@ load_dotenv(dotenv_path=f"{REL_PATH}.env")
 
 API_ENDPOINT = "https://api.sheety.co/3955488f8ee023601d308d21b9639dd3/flightDeals/prices"
 API_KEY = os.getenv("SHEETY_APIKEY")
-API_HEADERS = {
-    "Authorization": API_KEY,
-    "Content-Type": "application/json"
-}
+API_HEADERS = {"Authorization": API_KEY, "Content-Type": "application/json"}
+SHEET_NAME = "prices"
+SHEET_NAME_SINGULAR = "price"
 
 class DataManager:
     def __init__(self) -> None:
         pass
+
     def get_sheet_data(self):
         response = requests.get(url=API_ENDPOINT, headers=API_HEADERS)
         response.raise_for_status()
-        return response.json()["prices"]
-    def update_data(self, item_id, item_info):        
-        json_body = {"price":{"iataCode":item_info}}
-        print(json_body)
-        response = requests.put(url=f"{API_ENDPOINT}/{item_id}", headers=API_HEADERS, json=json_body)
-        response.raise_for_status()        
+        return response.json()[SHEET_NAME]
+
+    def update_data(self, item_id, item_key, item_value):
+        json_body = {SHEET_NAME_SINGULAR: {item_key: item_value}}
+        response = requests.put(url=f"{API_ENDPOINT}/{item_id}",
+                                headers=API_HEADERS,
+                                json=json_body)
+        response.raise_for_status()
--- 02-intermediate/24-flight-deals-start/main.py
+++ 02-intermediate/24-flight-deals-start/main.py
@@ -9,18 +9,25 @@ from flight_search import FlightSearch
 data_manager = DataManager()
 flight_search = FlightSearch()
 
-# getting flights sheet data
+# getting sheet data
+print("getting sheet data...")
 sheet_data = data_manager.get_sheet_data()
 
-# getting all iata codes on sheet data
-iata_codes = [data["iataCode"] for data in sheet_data]
-
 # checking if any iata code is empty
-if "" in iata_codes:
+if "" in [data["iataCode"] for data in sheet_data]:
     # getting missing iata codes
     for data in sheet_data:
-        iata_code = flight_search.get_iata_code(city_name=data["city"])
-        data["iataCode"] = iata_code
+        print("getting missing iata code...")
+        data["iataCode"] = flight_search.get_iata_code(city_name=data["city"])
+    # update sheet
+    for data in sheet_data:
+        print("update iata code on sheet...")
+        data_manager.update_data(item_id=data["id"],
+                                 item_key="iataCode",
+                                 item_value=data["iataCode"])
+else:
+    print("iata codes ok")
 
-for data in sheet_data:    
-    data_manager.update_data(item_id=data["id"],item_info=data["iataCode"])
+print(sheet_data)
+
+
