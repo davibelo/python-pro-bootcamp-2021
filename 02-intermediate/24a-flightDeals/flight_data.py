b62455d765010c106ee2964ec1b8728e69bec1f5
davibelo <davibelo@gmail.com>
2021-05-02 08:23:59 -0300

    formatting

--- 02-intermediate/24a-flightDeals/flight_data.py
+++ 02-intermediate/24a-flightDeals/flight_data.py
@@ -1,7 +1,9 @@
 import datetime as dt
 
+
 class FlightData:
-    def __init__(self, departure_city, departure_airport_code, arrival_city, arrival_airport_code, departure_date, arrival_date, price):
+    def __init__(self, departure_city, departure_airport_code, arrival_city,
+                 arrival_airport_code, departure_date, arrival_date, price):
         self.departure_city = departure_city
         self.departure_airport_code = departure_airport_code
         self.arrival_city = arrival_city
