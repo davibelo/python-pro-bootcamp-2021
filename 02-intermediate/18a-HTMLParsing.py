425067a38898bd6aa4cb239550c152edaa6480fd
davibelo <davibelo@gmail.com>
2021-05-04 14:06:01 -0300

    examples

--- 02-intermediate/18a-HTMLParsing.py
+++
@@ -1,9 +0,0 @@
-import os
-from bs4 import BeautifulSoup
-
-# https://www.crummy.com/software/BeautifulSoup/
-# pip install beautifulsoup4
-
-# getting actual directory and making a rel path
-REL_PATH = f"{os.path.dirname(__file__)}/"
-
