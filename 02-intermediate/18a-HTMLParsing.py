00ff3fcc7922eaff1fdcf1884e8518699a0bfcda
davibelo <davibelo@gmail.com>
2021-05-03 22:06:32 -0300

    html parser initial

---
+++ 02-intermediate/18a-HTMLParsing.py
@@ -0,0 +1,9 @@
+import os
+from bs4 import BeautifulSoup
+
+# https://www.crummy.com/software/BeautifulSoup/
+# pip install beautifulsoup4
+
+# getting actual directory and making a rel path
+REL_PATH = f"{os.path.dirname(__file__)}/"
+
