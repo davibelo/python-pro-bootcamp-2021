9f5d0f6b46b314f048b8eb11e634678f0352c23f
davibelo <davibelo@gmail.com>
2021-05-13 12:17:41 -0300

    starting

---
+++ 02-intermediate/25-WebScraping/100GreatMovies.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+print(soup.prettify())
