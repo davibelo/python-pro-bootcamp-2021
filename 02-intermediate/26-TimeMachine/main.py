90b62cadb9cd4811b5f710cad32598fd39b7fff5
davibelo <davibelo@gmail.com>
2021-05-22 11:33:41 -0300

    time machine spotify query done

--- 02-intermediate/26-TimeMachine/main.py
+++ 02-intermediate/26-TimeMachine/main.py
@@ -1,25 +1,56 @@
 from bs4 import BeautifulSoup
 import requests
+from dotenv import load_dotenv
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# --- SCRAPING BILLBOARD WEBSITE --- #
 
 # date = input("Type the date you want to come back (YYYY-MM-DD):")
 date = "2010-12-09"
 
-URL = f"https://www.billboard.com/charts/hot-100/{date}"
+BILLBOARD_URL = f"https://www.billboard.com/charts/hot-100/{date}"
 
-response = requests.get(URL)
+response = requests.get(BILLBOARD_URL)
 webpage = response.text
 
 soup = BeautifulSoup(webpage, "html.parser")
 
 songs_tags = soup.find_all(name="span",
                            class_="chart-element__information__song")
-artists_tags = soup.find_all(name="span",
-                             class_="chart-element__information__artist")
 
+PLAYLIST_SIZE = 30
 
-songs = [song.getText() for song in songs_tags]
-artists = [artist.getText() for artist in artists_tags]
+song_names = [song.getText() for song in songs_tags[:PLAYLIST_SIZE]]
+print(song_names)
 
-print(songs)
-print(artists)
+# --- ACCESSING SPOTIFY WEB API --- #
+
+# getting actual directory and making a rel path
+REL_PATH = f"{os.path.dirname(__file__)}/"
+
+# loading environment variables
+load_dotenv(dotenv_path=f"{REL_PATH}.env")
+
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+REDIRECT_URI = "http://localhost"
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
+                              client_secret=SPOTIFY_CLIENT_SECRET,
+                              redirect_uri=REDIRECT_URI,
+                              scope="playlist-modify-private"))
+user_id = sp.current_user()["id"]
+
+song_URIs = []
+for song in song_names:
+    results = sp.search(q=f"track:{song} year:{date[:4]}", limit=1, type="track")
+    try:
+        song_URIs.append(results["tracks"]["items"][0]["uri"])
+    except IndexError:
+        continue
+
+print(song_URIs)
 
