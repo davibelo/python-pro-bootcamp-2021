532a0e095cc66205d0b2da36c7353fe12f522544
davibelo <davibelo@gmail.com>
2021-05-30 19:03:45 -0300

    upgrades

--- 02-intermediate/28-CookieClicker/main.py
+++ 02-intermediate/28-CookieClicker/main.py
@@ -12,6 +12,8 @@ cookie = driver.find_element_by_id("cookie")
 game_timeout = time() + 60 * 5
 buy_item_timeout = time() + 5
 
+# --- Getting Upgrades --- #
+
 item_tags = driver.find_elements_by_css_selector("#store div")
 item_ids = [item.get_attribute("id") for item in item_tags]
 
@@ -21,8 +23,11 @@ item_price_texts = [item.text for item in item_price_tags]
 item_prices = []
 for item in item_price_texts:
     if item != "":
-        item_prices.append(item.split("-")[1].strip())
+        item_prices.append(int(item.split("-")[1].strip().replace(",", "")))
 
-print(item_prices)
+upgrades = {}
+for i in range(0, len(item_prices)):
+    upgrades[item_ids[i]] = item_prices[i]
+
 
 driver.quit()
