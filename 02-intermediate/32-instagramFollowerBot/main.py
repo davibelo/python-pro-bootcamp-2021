0aa265fc5463e665ee4904f134084b270e44bba3
davibelo <davibelo@gmail.com>
2021-06-24 18:49:29 -0300

    v2

--- 02-intermediate/32-instagramFollowerBot/main.py
+++ 02-intermediate/32-instagramFollowerBot/main.py
@@ -58,8 +58,9 @@ class InstaFollower:
         all_buttons = self.driver.find_elements_by_css_selector("li button")
         for button in all_buttons:
             try:
-                button.click()
+                button.click()                
                 time.sleep(1)
+            # exception in case the account is already followed
             except ElementClickInterceptedException:
                 cancel_button = self.driver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]')
                 cancel_button.click()
