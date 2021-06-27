a34388350db4e3cc0bb54c3e96f4791c02f8bdeb
davibelo <davibelo@gmail.com>
2021-06-26 22:34:04 -0300

    scrapping done

--- 02-intermediate/33-RealStateFinderBot/main.py
+++ 02-intermediate/33-RealStateFinderBot/main.py
@@ -1,35 +1,71 @@
 import os
-from dotenv import load_dotenv
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-# from selenium.common.exceptions import ElementClickInterceptedException
+import pandas as pd
 
 REL_PATH = os.path.dirname(__file__)
 CHROME_DRIVER_PATH = "/home/davibelo/chromedriver"
-URL = "https://rj.olx.com.br/rio-de-janeiro-e-regiao/zona-oeste/jacarepagua/imoveis/aluguel/apartamentos?gsp=1&pe=6000&ros=3&sp=2"
+URL = "https://rj.olx.com.br/rio-de-janeiro-e-regiao/zona-oeste/jacarepagua/imoveis/aluguel/apartamentos?pe=5000&ps=2000&ros=3&sp=2&ss=3"
 
 driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
 driver.get(URL)
 
-# # saving page source to use it with beautiful soup
-# webpage = driver.page_source
-# soup = BeautifulSoup(webpage, "html.parser")
+titles_raw = []
+links_raw = []
+neighborhoods_raw = []
 
-# # saving source HTML to check if selenium got page source correctly
-# prettyHTML = soup.prettify()
-# with open(f"{REL_PATH}/website.html", mode="w") as file:
-#     file.write(prettyHTML)
+titles = []
+links = []
+neighborhoods = []
 
 
-link_tags = driver.find_elements_by_css_selector("ul#ad-list li a")
+def get_info():
+    """
+    get information on loaded page
+    """
+    anchor_tags = driver.find_elements_by_css_selector("ul#ad-list li a")
+    for anchor_tag in anchor_tags:
+        title = anchor_tag.get_attribute("title")
+        if title == "":
+            pass
+        else:
+            titles_raw.append(title)
+    for anchor_tag in anchor_tags:
+        link = anchor_tag.get_attribute("href")
+        if not "olx.com.br" in link:
+            pass
+        else:
+            links_raw.append(link)
 
-titles = [tag.get_attribute("title") for tag in link_tags]
-links = [tag.get_attribute("href") for tag in link_tags]
-# title = link_tags[0].get_attribute("title")
-# link = link_tags[0].get_attribute("href")
+    span_tags = driver.find_elements_by_css_selector("ul#ad-list li a span")
+
+    for span_tag in span_tags:
+        if not "Rio de Janeiro, " in span_tag.text:
+            pass
+        else:
+            neighborhoods_raw.append(span_tag.text)
+
+
+def print_lengths():
+    print(len(titles_raw), len(links_raw), len(neighborhoods_raw))
+
+
+# --- scrapping --- #
+
+get_info()
+print_lengths()
+next_page_button = driver.find_element_by_link_text("Próxima pagina")
+next_page_button.click()
+get_info()
+print_lengths()
+
+driver.close()
+
+for i in range(len(titles_raw)):    
+    if "Jacare" in neighborhoods_raw[i]:
+        titles.append(titles_raw[i])
+        links.append(links_raw[i])
+        neighborhoods.append(neighborhoods_raw[i])
 
 print(titles)
 print(links)
-
-driver.close()
+print(neighborhoods)
