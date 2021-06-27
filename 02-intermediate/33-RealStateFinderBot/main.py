474fc3056048462e2010b5783c323dd62384710c
davibelo <davibelo@gmail.com>
2021-06-27 08:30:31 -0300

    generating results

--- 02-intermediate/33-RealStateFinderBot/main.py
+++ 02-intermediate/33-RealStateFinderBot/main.py
@@ -23,21 +23,22 @@ def get_info():
     get information on loaded page
     """
     anchor_tags = driver.find_elements_by_css_selector("ul#ad-list li a")
+    print("getting titles...")
     for anchor_tag in anchor_tags:
         title = anchor_tag.get_attribute("title")
         if title == "":
             pass
         else:
             titles_raw.append(title)
+    print("getting links...")
     for anchor_tag in anchor_tags:
         link = anchor_tag.get_attribute("href")
         if not "olx.com.br" in link:
             pass
         else:
             links_raw.append(link)
-
     span_tags = driver.find_elements_by_css_selector("ul#ad-list li a span")
-
+    print("getting neighborhoods...")
     for span_tag in span_tags:
         if not "Rio de Janeiro, " in span_tag.text:
             pass
@@ -46,10 +47,11 @@ def get_info():
 
 
 def print_lengths():
-    print(len(titles_raw), len(links_raw), len(neighborhoods_raw))
+    print("raw ads: ", len(titles_raw), len(links_raw), len(neighborhoods_raw))
+    print("filtered_ads: ",len(titles), len(links), len(neighborhoods))
 
 
-# --- scrapping --- #
+# --- START --- #
 
 get_info()
 print_lengths()
@@ -60,12 +62,29 @@ print_lengths()
 
 driver.close()
 
-for i in range(len(titles_raw)):    
-    if "Jacare" in neighborhoods_raw[i]:
+print("filtering information...")
+for i in range(len(titles_raw)):
+    if "Jacarepaguá" in neighborhoods_raw[i]:
         titles.append(titles_raw[i])
         links.append(links_raw[i])
         neighborhoods.append(neighborhoods_raw[i])
 
-print(titles)
-print(links)
-print(neighborhoods)
+print_lengths()
+# print(titles)
+# print(links)
+# print(neighborhoods)
+
+ads_dict = {
+    "titles": titles,
+    "neighborhood": neighborhoods,
+    "links": links
+}
+
+ads_df = pd.DataFrame.from_dict(ads_dict)
+print(ads_df.head())
+
+print("saving files...")
+writer = pd.ExcelWriter(f"{REL_PATH}/aptos.xlsx", engine="xlsxwriter")
+ads_df.to_excel(writer, sheet_name="Sheet1")
+writer.save()
+ads_df.to_csv(f"{REL_PATH}/aptos.csv")
