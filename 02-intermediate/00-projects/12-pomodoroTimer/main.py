b9aeb591fd9a30c2054af8425665920293d0bbfd
davibelo <davibelo@gmail.com>
2021-07-12 13:51:44 -0300

    correcting path

--- 02-intermediate/00-projects/12-pomodoroTimer/main.py
+++ 02-intermediate/00-projects/12-pomodoroTimer/main.py
@@ -83,7 +83,7 @@ title_label = Label(text="Timer", bg=YELLOW, fg=GREEN,
 title_label.grid(row=0, column=1)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file="02-intermediate/12-pomodoroTimer/tomato.png")
+tomato_img = PhotoImage(file="02-intermediate/00-projects/12-pomodoroTimer/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white",
                                 font=(FONT_NAME, 24, "bold"))
