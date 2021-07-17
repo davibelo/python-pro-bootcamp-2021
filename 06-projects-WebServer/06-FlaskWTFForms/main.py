726fa370026406a39e6f4c5a89119d4da2924bc1
davibelo <davibelo@gmail.com>
2021-07-17 16:58:58 -0300

    validartion features

--- 06-projects-WebServer/06-FlaskWTFForms/main.py
+++ 06-projects-WebServer/06-FlaskWTFForms/main.py
@@ -1,32 +1,31 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms.fields import PasswordField, SubmitField
-from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired
+from wtforms.fields import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
 
 # creating a new class for login form
 class LoginForm(FlaskForm):
-    email = EmailField(label="email: ")
-    password = PasswordField(label="password: ")
+    email = StringField(label="email: ", validators=[DataRequired(), Email()])
+    password = PasswordField(label="password: ", validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label="LOGIN")
+
 
 app = Flask(__name__)
 app.secret_key = "secretsecret"
+TEST_EMAIL = "d@d.com"
+TEST_PASS = "12345678"
 
-@app.route("/login", methods=["GET"])
+@app.route("/login", methods=["GET", "POST"])
 def login_route():
     # creating a login form object to pass through rendering
-    login_form = LoginForm()
+    login_form = LoginForm()    
+    if login_form.validate_on_submit():
+        if login_form.email.data == TEST_EMAIL and login_form.password.data == TEST_PASS:
+            return render_template("success.html")
+        else:
+            return render_template("failure.html")
     return render_template("login.html", login_form=login_form)
-
-
-@app.route("/login", methods=["POST"])
-def send_login():       
-    data = request.form
-    print(data["email"])
-    print(data["password"])
-    return "<h1>Successful submitted</h1>"
-
 
 
 if __name__ == "__main__":
