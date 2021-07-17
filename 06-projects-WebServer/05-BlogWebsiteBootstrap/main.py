67ed943d47071c77cabd2ef50f7cdd198fbf1add
davibelo <davibelo@gmail.com>
2021-07-17 12:15:31 -0300

    contact form feature

--- 06-projects-WebServer/05-BlogWebsiteBootstrap/main.py
+++ 06-projects-WebServer/05-BlogWebsiteBootstrap/main.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request
 from post import Post
+from dotenv import load_dotenv
 import requests
+import smtplib
+import os
+
+REL_PATH = f"{os.path.dirname(__file__)}"
+load_dotenv(dotenv_path=f"{REL_PATH}/.env")
+MY_EMAIL = os.getenv("MY_EMAIL")
+MY_EMAIL_PASSWORD = os.getenv("MY_EMAIL_PASSWORD")
+DEST_EMAIL = os.getenv("DEST_EMAIL")
 
 # making post objects using imported Post class
 posts_json = requests.get("https://api.npoint.io/ed99320662742443cc5b").json()
@@ -29,11 +38,24 @@ def contact_route():
 
 @app.route("/contact", methods=["POST"])
 def receive_contact_data():
-    data = request.form
-    print(data["name"])
-    print(data["email"])
-    print(data["phone"])
-    print(data["message"])
+    data = request.form    
+    name = data["name"]
+    email = data["email"]
+    phone = data["phone"]
+    message = data["message"]
+        
+    print("sending email...")
+    with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_EMAIL_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=DEST_EMAIL,
+            msg=
+            f"Subject:Contact from {name}\n\n{message}\n\n email: {email}\n phone: {phone}\n\n *this is from blog website contact form"
+        )
+    print(f"email sent to {DEST_EMAIL}!")
+
     return "<h1> Successful sent message </h1>"
 
 
