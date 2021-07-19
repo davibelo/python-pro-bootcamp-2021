3e14e5ee926bb753d02991827c014d751667f343
davibelo <davibelo@gmail.com>
2021-07-19 09:28:19 -0300

    simple refactoring

--- 06-projects-WebServer/07-CoffeeWifiAngela/main.py
+++ 06-projects-WebServer/07-CoffeeWifiAngela/main.py
@@ -4,7 +4,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
 import csv
-
 import os
 
 REL_PATH = f"{os.path.dirname(__file__)}"
@@ -15,14 +14,24 @@ Bootstrap(app)
 
 
 class CafeForm(FlaskForm):
-    cafe = StringField('Cafe name', validators=[DataRequired()])
-    location = StringField("Cafe Location on Google Maps (URL)", validators=[DataRequired(), URL()])
-    open = StringField("Opening Time e.g. 8AM", validators=[DataRequired()])
-    close = StringField("Closing Time e.g. 5:30PM", validators=[DataRequired()])
-    coffee_rating = SelectField("Coffee Rating", choices=["☕️", "☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"], validators=[DataRequired()])
-    wifi_rating = SelectField("Wifi Strength Rating", choices=["✘", "💪", "💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"], validators=[DataRequired()])
-    power_rating = SelectField("Power Socket Availability", choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    cafe = StringField(label="Cafe name", validators=[DataRequired()])
+    location = StringField(label="Cafe Location on Google Maps (URL)",
+                           validators=[DataRequired(), URL()])
+    open = StringField(label="Opening Time e.g. 8AM",
+                       validators=[DataRequired()])
+    close = StringField(label="Closing Time e.g. 5:30PM",
+                        validators=[DataRequired()])
+    coffee_rating = SelectField(label="Coffee Rating",
+                                choices=["☕️", "☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"],
+                                validators=[DataRequired()])
+    wifi_rating = SelectField(label="Wifi Strength Rating",
+                              choices=["✘", "💪", "💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"],
+                              validators=[DataRequired()])
+    power_rating = SelectField(
+        label="Power Socket Availability",
+        choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"],
+        validators=[DataRequired()])
+    submit = SubmitField(label="Submit")
 
 
 @app.route("/")
@@ -30,10 +39,10 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add', methods=["GET", "POST"])
+@app.route("/add", methods=["GET", "POST"])
 def add_cafe():
-    form = CafeForm()
-    if form.validate_on_submit():
+    form = CafeForm()    
+    if form.validate_on_submit(): # this method works only with POST request
         with open(f"{REL_PATH}/cafe-data.csv", mode="a") as csv_file:
             csv_file.write(f"\n{form.cafe.data},"
                            f"{form.location.data},"
@@ -42,11 +51,11 @@ def add_cafe():
                            f"{form.coffee_rating.data},"
                            f"{form.wifi_rating.data},"
                            f"{form.power_rating.data}")
-        return redirect(url_for('cafes'))
-    return render_template('add.html', form=form)
+        return redirect(url_for("cafes"))
+    return render_template("add.html", form=form)
 
 
-@app.route('/cafes')
+@app.route("/cafes")
 def cafes():
     with open(f"{REL_PATH}/cafe-data.csv", newline="") as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
