4a36f9c5536c5e477c65425e44d7dfe601f9b3ca
davibelo <davibelo@gmail.com>
2022-07-30 18:13:07 -0300

    creating db

--- 06-projects-WebServer/08-VirtualLibrary_UsingSQL/main.py
+++ 06-projects-WebServer/08-VirtualLibrary_UsingSQL/main.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+db = sqlite3.connect("books-colletion.db")
+cursor = db.cursor()
+
+cursor.execute(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)"
+)
 
 app = Flask(__name__)
 
@@ -22,6 +30,7 @@ def add():
         print(all_books)
         return redirect(url_for("home"))
     return render_template("add.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
