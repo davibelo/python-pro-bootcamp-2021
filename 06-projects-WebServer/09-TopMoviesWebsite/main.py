2d0beb0c614445b5b92c000ae5ae29c1df49ebc4
davibelo <davibelo@gmail.com>
2022-07-31 16:17:31 -0300

    getting edit info

--- 06-projects-WebServer/09-TopMoviesWebsite/main.py
+++ 06-projects-WebServer/09-TopMoviesWebsite/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, DecimalField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
@@ -32,6 +32,12 @@ class Movie(db.Model):
 db.create_all()
 
 
+# creating a edit form based on FlaskForm
+class MovieForm(FlaskForm):
+    rating = DecimalField(label="Your Rating 0-10:", places=1, validators=[DataRequired()])
+    review = StringField(label="Your review:", validators=[DataRequired()])
+    submit = SubmitField(label="Submit Review")
+
 @app.route("/")
 def home():
     return render_template("index.html", movies=db.session.query(Movie).all())
@@ -42,11 +48,15 @@ def add():
     return render_template("add.html")
 
 
-@app.route("/edit")
+@app.route("/edit", methods=["GET", "POST"])
 def edit():
-    movie_id = request.args.get("id")    
+    form = MovieForm()
+    if form.validate_on_submit(): # this method works only with POST request
+        print(form.rating.data)
+        print(form.review.data)
+    movie_id = request.args.get("id")
     movie_to_edit = Movie.query.get(movie_id)
-    return render_template("edit.html", movie=movie_to_edit)
+    return render_template("edit.html", movie=movie_to_edit, form=form)
 
 
 @app.route("/delete")
