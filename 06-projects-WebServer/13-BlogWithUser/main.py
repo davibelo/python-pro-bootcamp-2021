9046e46f3bfb7408b8feb9beb0909a65a900065a
davibelo <davibelo@gmail.com>
2022-11-04 22:23:33 -0300

    testing in linux env

--- 06-projects-WebServer/13-BlogWithUser/main.py
+++ 06-projects-WebServer/13-BlogWithUser/main.py
@@ -12,7 +12,8 @@ from datetime import date
 
 app = Flask(
     __name__,
-    instance_path="C:\\Users\\davib\\Desktop\\python-pro-bootcamp-2021\\06-projects-WebServer\\13-BlogWithUser"
+    # instance_path="C:\\Users\\davib\\Desktop\\python-pro-bootcamp-2021\\06-projects-WebServer\\13-BlogWithUser"
+    instance_path="/home/davibelo/Desktop/python-pro-bootcamp-2021/06-projects-WebServer/13-BlogWithUser"
 )
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
